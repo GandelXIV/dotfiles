ced6e138aefc1566db4980c147800241d35cf605
GandelXIV <filip@chovanec.com>
2023-10-19 22:02:24 +0200

    Init

---
+++ .config/aliases.sh
@@ -0,0 +1,6 @@
+alias ll='ls -l'
+alias nvim='nvim -c "set guicursor=a:block,i:ver10 | set relativenumber" '
+alias discord='discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy'
+alias gitree='git ls-tree -r --name-only HEAD | tree --fromfile'
+alias dockil='sudo docker kill (sudo docker container list | awk "{print $1}")'
+alias hx='helix'
