452c3a0c8856de1f0fdfae5c2602df8d0a70eb63
GandelXIV <filip@chovanec.com>
2024-03-21 19:37:45 +0100

    Override theme to archbox

--- .config/aliases.sh
+++ .config/aliases.sh
@@ -1,6 +1,13 @@
-alias ll='ls -l'
+# Fixes
 alias nvim='nvim -c "set guicursor=a:block,i:ver10 | set relativenumber" '
 alias discord='discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy'
-alias gitree='git ls-tree -r --name-only HEAD | tree --fromfile'
-alias dockil='sudo docker kill (sudo docker container list | awk "{print $1}")'
+
+# Common
+alias ll='ls -l'
 alias hx='helix'
+
+# Git
+alias gitree='git ls-tree -r --name-only HEAD | tree --fromfile'
+
+# Docker
+alias dockil='sudo docker kill (sudo docker container list | awk "{print $1}")'
